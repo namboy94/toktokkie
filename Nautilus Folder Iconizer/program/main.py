2490d65553fa35ea8cd6480ad4d0324eea5be442
Hermann Krumrey <uodxh@student.kit.edu>
2015-04-25 19:48:13 +0200

    Revert "Fixed Pythonpath in program.main.py"
    This reverts commit 808160b95eca0aad6c371c5a0715562a953ad8fc.

--- Nautilus Folder Iconizer/program/main.py
+++ Nautilus Folder Iconizer/program/main.py
@@ -8,12 +8,3 @@ Modified on Apr 25, 2015
 @author Hermann Krumrey
 @version 0.1
 '''
-
-#imports
-import sys
-
-#fix pythonpath
-splitPath = sys.argv[0].split("/")
-lengthToCut = len(splitPath[len(splitPath) - 1]) + len(splitPath[len(splitPath) - 2]) + 2
-upperDirectory = sys.argv[0][:-lengthToCut]
-sys.path.append(upperDirectory)
