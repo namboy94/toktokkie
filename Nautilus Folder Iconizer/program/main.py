feaa362f38e2e11a4441aed48495802634f8f689
Hermann Krumrey <uodxh@student.kit.edu>
2015-04-25 19:52:51 +0200

    Integrated directory changing method in main.

--- Nautilus Folder Iconizer/program/main.py
+++ Nautilus Folder Iconizer/program/main.py
@@ -11,9 +11,13 @@ Modified on Apr 25, 2015
 
 #imports
 import sys
+from program.utils.ParserCollection import directoryChangeParser
 
 #fix pythonpath
 splitPath = sys.argv[0].split("/")
 lengthToCut = len(splitPath[len(splitPath) - 1]) + len(splitPath[len(splitPath) - 2]) + 2
 upperDirectory = sys.argv[0][:-lengthToCut]
 sys.path.append(upperDirectory)
+
+configFileLocation = upperDirectory + "/program/data/config.txt"
+directoryChangeParser(configFileLocation)
