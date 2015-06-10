4c09332941d9ed2d7c9a620d506fed3f0626f28b
hermann <hermann@krumreyh.com>
2015-06-10 18:37:56 +0200

    Set up the project with initial folder structure and a barebones Main class.

---
+++ batch-episode-renamer/src/main/java/com/krumreyh/java/Main.java
@@ -0,0 +1,19 @@
+package com.krumreyh.java;
+
+/**
+ * Main Class of the batch episode renamer
+ * @author Hermann Krumrey
+ * @version 0.1
+ */
+public class Main {
+
+	/**
+	 * main method that starts the user interface and renames the episodes according to the
+	 * HK-plex-conform episode file format
+	 * @param args - command line parameter
+	 */
+	public static void main(String[] args) {
+		
+	}
+	
+}
