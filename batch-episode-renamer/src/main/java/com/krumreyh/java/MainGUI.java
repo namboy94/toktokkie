6e987c98e99ba174fe5fb2156e9155da49bde139
hermann <hermann@krumreyh.com>
2015-06-10 18:54:44 +0200

    Added barebones GUI class.

---
+++ batch-episode-renamer/src/main/java/com/krumreyh/java/MainGUI.java
@@ -0,0 +1,17 @@
+package com.krumreyh.java;
+
+/**
+ * Class that implements a GUI using Swing
+ * @author Hermann Krumrey
+ * @versio 0.1
+ */
+public class MainGUI {
+
+	/**
+	 * Constructor of the GUI that adds all UI elements
+	 */
+	public MainGUI() {
+		
+	}
+	
+}
