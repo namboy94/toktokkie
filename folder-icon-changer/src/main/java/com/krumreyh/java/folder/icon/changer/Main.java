a6dd1557a47d030efbc8483beabab791fdf4ff83
hermann <hermann@krumreyh.com>
2015-06-15 23:06:15 +0200

    Implemented Basic outline for the GUI.

--- folder-icon-changer/src/main/java/com/krumreyh/java/folder/icon/changer/Main.java
+++ folder-icon-changer/src/main/java/com/krumreyh/java/folder/icon/changer/Main.java
@@ -1,9 +1,20 @@
 package com.krumreyh.java.folder.icon.changer;
 
+import com.krumreyh.java.folder.icon.changer.userinterface.gui.MainGUI;
+
+/**
+ * Class that contains the main method of the program
+ * @author Hermann Krumrey
+ * @version 1.0
+ */
 public class Main {
 
+	/**
+	 * Main method that starts the program
+	 * @param args - command line parameters
+	 */
 	public static void main(String[] args) {
-		
+		new MainGUI();
 	}
 	
 }
---
+++ folder-icon-changer/src/main/java/com/krumreyh/java/folder/icon/changer/userinterface/gui/MainGUI.java
@@ -0,0 +1,37 @@
+package com.krumreyh.java.folder.icon.changer.userinterface.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import com.krumreyh.java.krumreylib.gui.swing.GUITemplate;
+
+/**
+ * Class that implements a basic GUI for the program
+ * @author Hermann Krumrey
+ * @version 1.0
+ */
+public class MainGUI extends GUITemplate{
+
+	/**
+	 * Constructor that adds all elements of the GUI and makes the GUI visible
+	 */
+	public MainGUI() {
+		this.setGUISettings("Folder Icon Changer", 400, 400, 400, 90, null, EXIT_ON_CLOSE, false);
+		this.addTextField("", 10, 10, 200, 40);
+		this.addButton("Start", 220, 10, 170, 40, null);
+		this.startGUI();
+	}
+	
+	/**
+	 * Starts the folder icon changing algorithm
+	 */
+	protected class StartButton implements ActionListener {
+
+		/**
+		 * The performed action on button-press
+		 */
+		public void actionPerformed(ActionEvent arg0) {
+			
+		}
+	}
+}
