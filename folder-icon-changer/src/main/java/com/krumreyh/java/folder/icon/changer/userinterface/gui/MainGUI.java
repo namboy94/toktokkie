aea62e1d476f1a3057932db182aeacd295d1d470
hermann <hermann@krumreyh.com>
2015-06-19 16:14:24 +0200

    Implemented ways to choose for which File Browser the icons should be changed.

--- folder-icon-changer/src/main/java/com/krumreyh/java/folder/icon/changer/userinterface/gui/MainGUI.java
+++ folder-icon-changer/src/main/java/com/krumreyh/java/folder/icon/changer/userinterface/gui/MainGUI.java
@@ -3,6 +3,8 @@ package com.krumreyh.java.folder.icon.changer.userinterface.gui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 import com.krumreyh.java.folder.icon.changer.iconizers.NautilusIconizer;
@@ -15,16 +17,31 @@ import com.krumreyh.java.krumreylib.gui.swing.GUITemplate;
  */
 public class MainGUI extends GUITemplate {
 
-	private JTextField directory;
+	protected JTextField directory;
+	protected JComboBox<String> selectedBrowser;
 	
 	/**
 	 * Constructor that adds all elements of the GUI and makes the GUI visible
 	 */
 	public MainGUI() {
+		
+		String[] validIconizers = null;
+		switch (System.getProperty("os.name")) {
+		case "Linux":	validIconizers = new String[] {"Nautilus", "Nemo"};
+						break;
+		case "Windows":	validIconizers = new String[] {"Explorer"};
+						break;
+		case "Mac":		validIconizers = new String[] {"Finder"};
+						break;
+		default:		showPopUpMessage("Unsupported OS detected");
+		}
+		
 		this.setGUISettings("Folder Icon Changer", 400, 400, 400, 170, null, EXIT_ON_CLOSE, false);
 		this.directory = this.addTextField("", 10, 10, 200, 40);
 		this.addButton("Start", 220, 10, 170, 40, new StartButton());
-		this.addLabel("OS", 220, 210, 170, 40);
+		this.selectedBrowser = this.addDropDownMenu(validIconizers, 170, 70, 200, 20);
+		this.addLabel("OS: " + System.getProperty("os.name"), 80, 60, 100, 40);
+		
 		this.startGUI();
 	}
 	
@@ -40,11 +57,21 @@ public class MainGUI extends GUITemplate {
 		public void actionPerformed(ActionEvent e) {
 			try {
 				switch (System.getProperty("os.name")) {
-				case "Linux":	new NautilusIconizer(MainGUI.this.directory.getText()).iconize();
-								break;
-				case "Windows":	break;
-				case "Mac":		break;
-				default:		showPopUpMessage("Unsupported OS detected");
+				case "Linux":		switch((String) MainGUI.this.selectedBrowser.getSelectedItem()) {
+				case "Nemo":		
+				case "Nautilus":	new NautilusIconizer(MainGUI.this.directory.getText()).iconize();
+									break;
+				default:			}
+									break;
+				case "Windows":		switch((String) MainGUI.this.selectedBrowser.getSelectedItem()) {
+				case "Explorer":	break;
+				default:			}
+									break;
+				case "Mac":			switch((String) MainGUI.this.selectedBrowser.getSelectedItem()) {
+				case "Finder":		break;
+				default:			}
+									break;
+				default:			showPopUpMessage("Unsupported OS detected");
 				}
 				showPopUpMessage("Operation completed");
 				
