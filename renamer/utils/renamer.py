78eb0faa6f30b4dc18264476f41d4f4cd75932d1
Hermann Krumrey <hermann@krumreyh.com>
2015-12-09 12:06:54 +0100

    Re-added Renamer

---
+++ renamer/utils/renamer.py
@@ -0,0 +1,11 @@
+import os
+
+"""
+@author Hermann Krumrey<hermann@krumreyh.com>
+method that renames a directory of episodes
+@:param directory - the directory to be used
+"""
+def renameDirEpisodes(directory):
+
+    showname = os.path.dirname(directory)
+    print(showname)
