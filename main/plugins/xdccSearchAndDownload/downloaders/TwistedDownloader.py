f73285f0b13ff898c16062289da3aedec10818c0
Hermann Krumrey <hermann@krumreyh.com>
2015-12-19 21:39:38 +0100

    Last commit before preparing baggage for USA.

---
+++ main/plugins/xdccSearchAndDownload/downloaders/TwistedDownloader.py
@@ -0,0 +1,13 @@
+"""
+Wrapper for Gregory Eric Sanderson Turcot Temlett MacDonnell Forbes's XDCC Downloader
+@author Hermann Krumrey<hermann@krumreyh.com>
+"""
+class TwistedDownloader(object):
+
+    """
+    Constructor
+    """
+    def __init__(self, packs):
+
+
+    def downloadLoop(self):
