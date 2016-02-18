65e9d079bdc4f1f9a6343f922a7a9553d8312815
Hermann Krumrey <hermann@krumreyh.com>
2016-02-18 01:44:25 +0100

    Started setup.py

---
+++ setup.py
@@ -0,0 +1,67 @@
+"""
+Copyright 2015,2016 Hermann Krumrey
+
+This file is part of media-manager.
+
+    media-manager is a program that allows convenient managing of various
+    local media collections, mostly focused on video.
+
+    media-manager is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    media-manager is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with media-manager.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+# imports
+from setuptools import setup
+
+
+def readme():
+    """
+    Reads the readme file.
+    :return: the readme file as a string
+    """
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='media-manager',
+      version='0.1',
+      description='A personal media manager program',
+      long_description=readme(),
+      classifiers=['Development Status :: 2 - Pre-Alpha',
+                   'Intended Audience :: End Users/Desktop',
+                   'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                   'Programming Language :: Python :: 3',
+                   'Topic :: Utilities',
+                   'Natural Language :: English',
+                   'Operating System :: POSIX :: Linux'
+                   ],
+      url='http://namibsun.net/namboy94/media-manager',
+      author='Hermann Krumrey',
+      author_email='hermann@krumreyh.com',
+      license='GNU GPL3',
+      packages=['media_manager',
+                'media_manager.external',
+                'media_manager.guitemplates',
+                'media_manager.guitemplates.gtk',
+                'media_manager.mainuserinterfaces',
+                'media_manager.parsers',
+                'media_manager.plugins',
+                'media_manager.plugins',
+                'media_manager.startup',
+                'media_manager.startup.scripts'],
+      install_requires=[],
+      dependency_links=['https://git.gnome.org/browse/pygobject'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      scripts=['bin/financemanager'],
+      zip_safe=False)
