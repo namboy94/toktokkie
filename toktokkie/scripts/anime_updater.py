5954e14a87b69b088ac873b1f8b2610859f189c0
Hermann Krumrey <hermann@krumreyh.com>
2016-07-07 18:36:13 +0200

    Updated License text

--- toktokkie/scripts/anime_updater.py
+++ toktokkie/scripts/anime_updater.py
@@ -1,27 +1,24 @@
-#!/usr/bin/python3
 """
 LICENSE:
-
 Copyright 2015,2016 Hermann Krumrey
 
-This file is part of media-manager.
+This file is part of toktokkie.
 
-    media-manager is a program that allows convenient managing of various
+    toktokkie is a program that allows convenient managing of various
     local media collections, mostly focused on video.
 
-    media-manager is free software: you can redistribute it and/or modify
+    toktokkie is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    media-manager is distributed in the hope that it will be useful,
+    toktokkie is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with media-manager.  If not, see <http://www.gnu.org/licenses/>.
-
+    along with toktokkie.  If not, see <http://www.gnu.org/licenses/>.
 LICENSE
 """
 
