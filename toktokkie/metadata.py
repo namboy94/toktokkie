f58e3008a237f6f7c6f9c3adb27028c3d0cf0474
Hermann Krumrey <hermann@krumreyh.com>
2016-07-23 22:59:24 +0200

    Version bump

--- toktokkie/metadata.py
+++ toktokkie/metadata.py
@@ -37,7 +37,7 @@ project_description = "A personal media manager program"
 A short description of the project
 """
 
-version_number = "0.9.9.1"
+version_number = "0.9.9.2"
 """
 The current version of the program.
 """
