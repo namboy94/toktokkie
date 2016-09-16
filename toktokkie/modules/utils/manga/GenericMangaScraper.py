e4ba8875a3932d989ee72300cf12c1a0ac50710f
Hermann Krumrey <hermann@krumreyh.com>
2016-09-16 18:47:20 +0200

    Implemented the structure for a Manga scraper

--- toktokkie/modules/utils/manga/GenericMangaScraper.py
+++ toktokkie/modules/utils/manga/GenericMangaScraper.py
@@ -21,3 +21,31 @@ This file is part of toktokkie.
     along with toktokkie.  If not, see <http://www.gnu.org/licenses/>.
 LICENSE
 """
+
+# imports
+from typing import List
+from toktokkie.modules.objects.manga.MangaVolume import MangaVolume
+
+
+class GenericMangaScraper(object):
+    """
+    Class that models how a Manga Scraper should operate
+    """
+
+    def url_match(self, manga_url: str) -> bool:
+        """
+        Chacks if a URL matches the pattern expected by the scraper
+
+        :param manga_url: the URL to check
+        :return: True if it matches, False otherwise
+        """
+        raise NotImplementedError()
+
+    def scrape_volumes_from_url(self, manga_url) -> List[MangaVolume]:
+        """
+        Scrapes a given URL
+
+        :param manga_url: the given URL to scrape
+        :return: a list of volumes, which should also contain chapters
+        """
+        raise NotImplementedError()
