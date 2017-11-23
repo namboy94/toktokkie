fbd1683aa1f260772ed9454b822379e1ed0a61a1
Hermann Krumrey <hermann@krumreyh.com>
2017-11-23 12:20:50 +0100

    Adjust metadata

--- setup.py
+++ setup.py
@@ -1,75 +1,75 @@
 """
-LICENSE:
 Copyright 2015-2017 Hermann Krumrey
 
 This file is part of toktokkie.
 
-    toktokkie is a program that allows convenient managing of various
-    local media collections, mostly focused on video.
+toktokkie is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    toktokkie is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+toktokkie is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    toktokkie is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with toktokkie.  If not, see <http://www.gnu.org/licenses/>.
-LICENSE
+You should have received a copy of the GNU General Public License
+along with toktokkie.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 # imports
 import os
 import json
-from toktokkie.metadata import version
+from toktokkie import version
 from setuptools import setup, find_packages
 
 
 def readme():
     """
-    Reads the readme file.
-
+    Reads the readme file and converts it to RST if pypandoc is
+    installed. If not, the raw markdown text is returned
     :return: the readme file as a string
     """
     # noinspection PyBroadException
     try:
-        # noinspection PyPackageRequirements
+        # noinspection PyPackageRequirements,PyUnresolvedReferences
         import pypandoc
-        with open('README.md') as f:
+        with open("README.md") as f:
             # Convert markdown file to rst
             markdown = f.read()
-            rst = pypandoc.convert(markdown, 'rst', format='md')
+            rst = pypandoc.convert(markdown, "rst", format="md")
             return rst
-    except:
+
+    except ModuleNotFoundError:
         # If pandoc is not installed, just return the raw markdown text
-        with open('README.md') as f:
+        with open("README.md") as f:
             return f.read()
 
 
 def find_scripts():
     """
     Returns a list of scripts in the bin directory
-
     :return: the list of scripts
     """
-    try:
-        scripts = []
-        for file_name in os.listdir("bin"):
-            if not file_name == "__init__.py" and os.path.isfile(os.path.join("bin", file_name)):
-                scripts.append(os.path.join("bin", file_name))
-        return scripts
-    except OSError:
-        return []
+    scripts = []
+
+    for file_name in os.listdir("bin"):
+
+        path = os.path.join("bin", file_name)
+        if file_name == "__init__.py":
+            continue
+        elif not os.path.isfile(path):
+            continue
+        else:
+            scripts.append(os.path.join("bin", file_name))
+
+    return scripts
 
 
 def create_local_config_dir():
     """
-    Creates alocal configuration directory with default values if none exist yet
-    
+    Creates a local configuration directory with default values if none exist
+    yet
     :return: None
     """
     toktokkie_dir = os.path.join(os.path.expanduser("~"), ".toktokkie")
@@ -83,11 +83,12 @@ def create_local_config_dir():
 
 
 create_local_config_dir()
-setup(name="toktokkie",
-      version=version,
-      description="A personal media manager program",
-      long_description=readme(),
-      classifiers=[
+setup(
+    name="toktokkie",
+    version=version,
+    description="A personal media manager program",
+    long_description=readme(),
+    classifiers=[
         "Environment :: Other Environment",
         "Natural Language :: English",
         "Intended Audience :: End Users/Desktop",
@@ -97,15 +98,24 @@ setup(name="toktokkie",
         "Programming Language :: Python :: 2",
         "Topic :: Utilities",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
-      ],
-      url="https://gitlab.namibsun.net/namboy94/toktokkie",
-      download_url="https://gitlab.namibsun.net/namboy94/toktokkie/repository/archive.zip?ref=master",
-      author="Hermann Krumrey",
-      author_email="hermann@krumreyh.com",
-      license="GNU GPL3",
-      packages=find_packages(),
-      install_requires=['tvdb_api', 'beautifulsoup4', 'typing', 'raven', 'urwid', 'xdcc_dl'],
-      test_suite='nose.collector',
-      tests_require=['nose'],
-      scripts=find_scripts(),
-      zip_safe=False)
+    ],
+    url="https://gitlab.namibsun.net/namboy94/toktokkie",
+    download_url="https://gitlab.namibsun.net/namboy94/toktokkie/"
+                 "repository/archive.zip?ref=master",
+    author="Hermann Krumrey",
+    author_email="hermann@krumreyh.com",
+    license="GNU GPL3",
+    packages=find_packages(),
+    install_requires=[
+        'tvdb_api',
+        'beautifulsoup4',
+        'typing',
+        'raven',
+        'urwid',
+        'xdcc_dl'
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    scripts=find_scripts(),
+    zip_safe=False
+)
