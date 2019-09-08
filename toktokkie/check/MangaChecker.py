f681442fd75f49826fe95b80854ff47025067a7e
Hermann Krumrey <hermann@krumreyh.com>
2019-09-09 01:33:22 +0200

    Find way to more or less guess what the newest manga chapter could be for any given anilist series

--- toktokkie/check/MangaChecker.py
+++ toktokkie/check/MangaChecker.py
@@ -17,11 +17,118 @@ You should have received a copy of the GNU General Public License
 along with toktokkie.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import os
+import json
+import requests
+from typing import Optional
 from toktokkie.check.Checker import Checker
+from toktokkie.metadata.Manga import Manga
+from toktokkie.metadata.components.enums import MangaIdType
+from anime_list_apis.api.AnilistApi import AnilistApi
 
 
 class MangaChecker(Checker):
     """
     Class that check Manga media for consistency
     """
-    pass
+
+    def check(self) -> bool:
+        """
+        Performs sanity checks and prints out anything that's wrong
+        :return: The result of the check
+        """
+        valid = super().check()
+        if self.config.get("anilist_user") is not None:
+            valid = self._check_chapter_progress() and valid
+        return valid
+
+    def _check_icons(self) -> bool:
+        """
+        Only checks for a main.png icon file.
+        :return: The result of the check
+        """
+        valid = True
+
+        if not os.path.isdir(self.metadata.icon_directory):
+            valid = self.error("Missing icon directory")
+
+        main_icon = os.path.join(self.metadata.icon_directory, "main.png")
+        if not os.path.isfile(main_icon):
+            valid = self.error("Missing main icon file for {}".format(
+                self.metadata.name
+            ))
+
+        return valid
+
+    def _check_chapter_progress(self) -> bool:
+        """
+        Checks the chapter progress using the best guess anilist user data
+        can give us.
+        :return: The result of the check
+        """
+        # noinspection PyTypeChecker
+        metadata = self.metadata  # type: Manga
+
+        local_chaptercount = len(os.listdir(metadata.main_path))
+
+        try:
+            anilist_id = int(metadata.ids.get(MangaIdType.ANILIST)[0])
+            remote_chaptercount = self._guess_latest_chapter(int(anilist_id))
+            if remote_chaptercount is None:
+                return True
+            else:
+                complete = remote_chaptercount == local_chaptercount
+                if not complete:
+                    self.warn("Local chapters and available chapters "
+                              "don't match: Local: {} / Available: {}"
+                              .format(local_chaptercount, remote_chaptercount))
+                return complete
+        except (IndexError, ValueError):
+            return True
+
+    def _guess_latest_chapter(self, anilist_id: int) -> Optional[int]:
+        """
+        Guesses the latest chapter number based on anilist user activity
+        :param anilist_id: The anilist ID to check
+        :return: The latest chapter number
+        """
+        api = self.config["anilist_api"]  # type: AnilistApi
+        info = api.get_manga_data(anilist_id)
+
+        chapter_count = info.chapter_count
+        if chapter_count is None:
+            query = """
+            query ($id: Int) {
+              Page(page: 1) {
+                activities(mediaId: $id, sort: ID_DESC) {
+                  ... on ListActivity {
+                    progress
+                    userId
+                  }
+                }
+              }
+            }
+            """
+            resp = requests.post(
+                "https://graphql.anilist.co",
+                json={"query": query, "variables": {"id": anilist_id}}
+            )
+            data = json.loads(resp.text)["data"]["Page"]["activities"]
+
+            progresses = []
+            for entry in data:
+                progress = entry["progress"]
+                if progress is not None:
+                    progress = entry["progress"].split(" - ")[-1]
+                    progresses.append(progress)
+
+            progresses.sort()
+
+            best_guess = progresses.pop(-1)
+            while progresses.count(best_guess) < 1:
+                best_guess = progresses.pop(-1)
+
+            chapter_count = best_guess
+
+        print(chapter_count)
+        return chapter_count
