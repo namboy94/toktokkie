61557037528b0bf8bb8003d7fdaaae81d86b777c
Hermann Krumrey <hermann@krumreyh.com>
2019-10-25 16:47:02 +0200

    Add songs and music videos to music albums

--- toktokkie/metadata/types/components/MusicAlbum.py
+++ toktokkie/metadata/types/components/MusicAlbum.py
@@ -18,7 +18,11 @@ along with toktokkie.  If not, see <http://www.gnu.org/licenses/>.
 LICENSE"""
 
 import os
-from typing import Dict, Any, List
+import logging
+import mimetypes
+from typing import Dict, Any, List, Optional, Tuple
+from mutagen.easyid3 import EasyID3
+from puffotter.os import listdir, get_ext
 from toktokkie.metadata.types.components.Component import Component
 from toktokkie.metadata.ids.IdType import IdType
 from toktokkie.metadata.ids.functions import objectify_ids, stringify_ids, \
@@ -42,8 +46,12 @@ class MusicAlbum(Component):
         :param parent_ids: The IDs associated with the parent
         :param json_data: The JSON data of the album
         """
+        self.logger = logging.getLogger(self.__class__.__name__)
+
         self.parent_path = parent_path
         self.parent_ids = parent_ids
+
+        self.artist_name = os.path.basename(os.path.normpath(parent_path))
 
         self.name = json_data["name"]
         self.genre = json_data["genre"]
@@ -65,3 +73,236 @@ class MusicAlbum(Component):
             "year": self.year,
             "ids": stringify_ids(minimize_ids(self.ids, self.parent_ids))
         }
+
+    @property
+    def songs(self) -> List["MusicSong"]:
+        """
+        :return: All songs in this album
+        """
+        song_files = self.__get_files("audio")
+        return list(map(lambda x: MusicSong(x, self), song_files))
+
+    @property
+    def videos(self) -> List["MusicVideo"]:
+        """
+        :return: All music videos in this album
+        """
+        video_files = self.__get_files("video")
+        return list(map(lambda x: MusicVideo(x, self), video_files))
+
+    def __get_files(self, mimetype: str) -> List[str]:
+        """
+        Retrieves the files in the album directory based on their mime type
+        :param mimetype: The mime type of the files to find
+        :return: The files with that mimetype
+        """
+
+        files = []
+        for _file, path in listdir(self.path, no_dirs=True):
+            guess = str(mimetypes.MimeTypes().guess_type(path)[0])
+            if guess.startswith(mimetype):
+                files.append(path)
+        return files
+
+
+class MusicSong:
+    """
+    Class that models a single song
+    """
+
+    def __init__(self, path: str, album: MusicAlbum):
+        """
+        Initializes the object
+        :param path: The path to the song file
+        :param album: The album this song is a part of
+        """
+
+        self.logger = logging.getLogger(self.__class__.__name__)
+
+        self.album = album
+        self.path = path
+        self.format = get_ext(self.path)
+
+        if self.format == "mp3":
+            self._tags = self.__load_mp3_tags()
+        else:
+            self._tags = {}  # type: Dict[str, str]
+
+    def save_tags(self):
+        """
+        Saves any edited tags
+        :return: None
+        """
+        if self.format == "mp3":
+            self.__write_mp3_tags()
+        else:
+            pass
+
+    def __load_mp3_tags(self) -> Dict[str, str]:
+        """
+        Loads MP3 tags if this is an mp3 file
+        :return: The mp3 tags as a dictionary
+        """
+        if self.format != "mp3":
+            return {}
+
+        tags = dict(EasyID3(self.path))
+        for key in tags:
+            tag = tags[key]
+            if isinstance(tag, list):
+                if len(tag) >= 1:
+                    tags[key] = tag[0]
+                else:
+                    tags[key] = ""
+        return tags
+
+    def __write_mp3_tags(self):
+        """
+        Writes the current tags to the file as ID3 tags, if this is an mp3 file
+        :return: None
+        """
+        if self.format != "mp3":
+            self.logger.warning("Can't set mp3 tags for {}: not an mp3 file"
+                                .format(self.path))
+            return
+
+        mp3 = EasyID3(self.path)
+        for key, tag in self._tags:
+            if tag == "":
+                if key in mp3:
+                    mp3.pop(key)
+            else:
+                mp3[key] = tag
+        mp3.save()
+
+    @property
+    def title(self) -> str:
+        """
+        :return: The title of the song
+        """
+        return self._tags.get("title", os.path.basename(self.path))
+
+    @title.setter
+    def title(self, title: str):
+        """
+        :param title: The title of the song
+        :return: None
+        """
+        self._tags["title"] = title
+
+    @property
+    def artist_name(self) -> Optional[str]:
+        """
+        :return: The song's artist name
+        """
+        return self._tags.get("artist")
+
+    @artist_name.setter
+    def artist_name(self, name: str):
+        """
+        :param name: The song's artist name
+        :return: None
+        """
+        self._tags["artist"] = name
+
+    @property
+    def album_artist_name(self) -> Optional[str]:
+        """
+        :return: The song's album artist name
+        """
+        return self._tags.get("album_artist")
+
+    @album_artist_name.setter
+    def album_artist_name(self, name: str):
+        """
+        :param name: The song's album artist name
+        :return: None
+        """
+        self._tags["album_artist"] = name
+
+    @property
+    def album_name(self) -> str:
+        """
+        :return: The song's album name
+        """
+        return self._tags.get("album", self.album.name)
+
+    @album_name.setter
+    def album_name(self, name: str):
+        """
+        :param name: The song's album name
+        :return: None
+        """
+        self._tags["album"] = name
+
+    @property
+    def genre(self) -> str:
+        """
+        :return: The song's genre
+        """
+        return self._tags.get("genre", self.album.genre)
+
+    @genre.setter
+    def genre(self, genre: str):
+        """
+        :param genre: The song's genre
+        :return: None
+        """
+        self._tags["genre"] = genre
+
+    @property
+    def tracknumber(self) -> Optional[Tuple[int, int]]:
+        """
+        :return: The song's track number as a tuple consisting of the song's
+                 track number and the total amount of tracks in the album
+        """
+        tracknumber = self._tags.get("tracknumber")
+        if tracknumber is not None:
+            track, total = tracknumber.split("/")
+            return int(track), int(total)
+        else:
+            return None
+
+    @tracknumber.setter
+    def tracknumber(self, track_number: Tuple[int, int]):
+        """
+        :param track_number: The song's track number as a tuple consisting of
+                             the song's track number and the total amount
+                             of tracks in the album
+        :return: None
+        """
+        track, total = track_number
+        self._tags["tracknumber"] = "{}/{}".format(track, total)
+
+    @property
+    def year(self) -> int:
+        """
+        :return: The year this song was released
+        """
+        return int(self._tags.get("date", self.album.year))
+
+    @year.setter
+    def year(self, year: int):
+        """
+        :param year: The year this song was released
+        :return: None
+        """
+        self._tags["date"] = str(year)
+
+
+class MusicVideo:
+    """
+    Class that keeps track of information for music videos
+    """
+
+    def __init__(self, path: str, album: MusicAlbum):
+        """
+        Initializes the music video
+        :param path: The path to the video file
+        :param album: The album to which the music video belongs to
+        """
+        self.logger = logging.getLogger(self.__class__.__name__)
+
+        self.album = album
+        self.path = path
+        self.format = get_ext(self.path)
