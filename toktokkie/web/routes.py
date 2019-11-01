afebc6456b3e15ab99790098261e4b8471087177
Hermann Krumrey <hermann@krumreyh.com>
2019-11-01 16:23:36 +0100

    Create web application that can reliably list toktokkie media directories

--- toktokkie/web/routes.py
+++ toktokkie/web/routes.py
@@ -18,22 +18,35 @@ along with toktokkie.  If not, see <http://www.gnu.org/licenses/>.
 LICENSE"""
 
 import os
-from flask import render_template, Response, request
+from flask import render_template, Response, request, redirect, url_for
 from puffotter.os import get_ext
-from toktokkie.web import app
+from toktokkie.web import app, db
 from toktokkie.Directory import Directory
 from toktokkie.web.models.MediaLocation import MediaLocation
+from toktokkie.metadata.MediaType import MediaType
 
 
 @app.route("/")
-def root():
+def index():
+    """
+    List all directories that were found in the stored content directories
+    :return: The response
+    """
     paths = [x.path for x in MediaLocation.query.all()]
+    mapped_dirs = {x: [] for x in MediaType}
 
-    media_dirs = []
     for path in paths:
-        media_dirs += Directory.load_directories(path)
+        directories = Directory.load_directories(path)
+        for directory in directories:
+            mapped_dirs[directory.metadata.media_type()].append(directory)
 
-    return render_template("list.html", media_dirs=media_dirs)
+    media_names = sorted([x for x in MediaType], key=lambda x: x.value)
+    media_dirs = []
+
+    for media_type in media_names:
+        media_dirs.append((media_type, mapped_dirs[media_type]))
+
+    return render_template("index.html", media_dirs=media_dirs)
 
 
 @app.route("/image/<image_format>")
@@ -50,3 +63,29 @@ def image(image_format: str) -> Response:
     else:
         with open(path, "rb") as f:
             return Response(f.read(), mimetype="image/" + image_format)
+
+
+@app.route("/content_dirs", methods=["GET"])
+def content_dirs():
+    paths = [x.path for x in MediaLocation.query.all()]
+    return render_template("content_dirs.html", paths=paths)
+
+
+@app.route("/delete_content_dir", methods=["POST"])
+def delete_content_dir():
+    target = request.form.get("target")
+    if target is not None:
+        location = MediaLocation.query.filter_by(path=target).first()
+        db.session.delete(location)
+        db.session.commit()
+    return redirect(url_for("content_dirs"))
+
+
+@app.route("/add_content_dir", methods=["POST"])
+def add_content_dir():
+    target = os.path.abspath(request.form.get("target"))
+    if target is not None and os.path.isdir(target):
+        location = MediaLocation(path=target)
+        db.session.add(location)
+        db.session.commit()
+    return redirect(url_for("content_dirs"))
